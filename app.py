d8e5e5de9e7bf9589bf6540f5bf49cb5c721d793
Gabriel <gabriel-bro@outlook.com>
2018-06-21 17:52:48 -0300

    detecção 2.0(webcam)

--- app.py
+++ app.py
@@ -1,31 +1,53 @@
 import cv2
 import sys
+import logging as log
+import datetime as dt
+from time import sleep
 
-# Get user supplied values
-imagePath = sys.argv[1]
 cascPath = "haarcascade_frontalface_default.xml"
-
-# Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
+log.basicConfig(filename='webcam.log',level=log.INFO)
 
-# Read the image
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+video_capture = cv2.VideoCapture(0)
+anterior = 0
 
-# Detect faces in the image
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    #flags = cv2.CV_HAAR_SCALE_IMAGE
-)
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
 
-print("Found {0} faces!".format(len(faces)))
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
 
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    if anterior != len(faces):
+        anterior = len(faces)
+        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
