b3e8b313142ab9709fdc265f68e0bbc5dee45d25
Gabriel Queiroz <gabrielqap@hotmail.com>
2018-06-25 14:45:39 -0300

    a

---
+++ controllers/AlertController.py
@@ -0,0 +1,39 @@
+
+class AlertController():
+	def __init__(self):
+		#Cria uma lista de alertas
+
+
+	def Imprime_Alerta(foragido, rosto, camera):
+		#Cria um alerta com esses parametros
+		#Chama a função que para imprimir o "alerta_foragido()", da classe Alerta.
+
+
+
+	#Função que gera um email sobre o alerta de foragido
+    def gera_gmail():
+    	# Credenciais
+		remetente    = 'seu-gmail@gmail.com'
+		senha        = 'sua-senha'
+		 
+		# Informações da mensagem
+		destinatario = 'email-do-destinatario@qualquercoisa.com'
+		assunto      = 'Alerta de Foragido'
+		texto        = _mensagem
+		 
+		# Preparando a mensagem
+		msg = '\r\n'.join([
+		  'From: %s' % remetente,
+		  'To: %s' % destinatario,
+		  'Subject: %s' % assunto,
+		  '',
+		  '%s' % texto
+		  ])
+		 
+		# Enviando o email
+		server = smtplib.SMTP('smtp.gmail.com:587')
+		server.starttls()
+		server.login(remetente,senha)
+		server.sendmail(remetente, destinatario, msg)
+		server.quit()
+		
--- models/alerta.py
+++ models/alerta.py
@@ -21,29 +21,4 @@ class Alerta(object):
     	return _mensagem
 
 
-    #Função que gera um email sobre o alerta de foragido
-    def gera_gmail():
-    	# Credenciais
-		remetente    = 'seu-gmail@gmail.com'
-		senha        = 'sua-senha'
-		 
-		# Informações da mensagem
-		destinatario = 'email-do-destinatario@qualquercoisa.com'
-		assunto      = 'Alerta de Foragido'
-		texto        = _mensagem
-		 
-		# Preparando a mensagem
-		msg = '\r\n'.join([
-		  'From: %s' % remetente,
-		  'To: %s' % destinatario,
-		  'Subject: %s' % assunto,
-		  '',
-		  '%s' % texto
-		  ])
-		 
-		# Enviando o email
-		server = smtplib.SMTP('smtp.gmail.com:587')
-		server.starttls()
-		server.login(remetente,senha)
-		server.sendmail(remetente, destinatario, msg)
-		server.quit()
+    
