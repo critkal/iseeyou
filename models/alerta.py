f4e38f02b83dcab945c0b1903cbeb67f128bcce8
Gabriel Queiroz <gabrielqap@hotmail.com>
2018-06-25 04:51:19 -0300

    alterado

--- models/alerta.py
+++ models/alerta.py
@@ -1,10 +1,49 @@
 import foragido
 import rosto
 import camera
+import smtplib
+from datetime import datetime
 
 class Alerta(object):
 
-    def __init__(self, mensagem, Foragido, nome, Rosto, Camera):
+    def __init__(self, Foragido, Rosto, Camera):
         self._mensagem = mensagem
         self._foragido = Foragido(nome, Rosto())
         self._camera = Camera()
+
+    
+
+    def alerta_foragido():
+    	now = datetime.now()
+    	_mensagem = "O foragido " + Foragido._nome + " foi reconhecido pela " + Camera._rua + "às " + str(now.hour)+":"
+    		+ str(now.minute) + ":" + str(now.second), "do dia" +  str(now.day) + " do mês " + str(now.month) + " do ano de " +
+    		str (now.year)
+    	return _mensagem
+
+
+    #Função que gera um email sobre o alerta de foragido
+    def gera_gmail():
+    	# Credenciais
+		remetente    = 'seu-gmail@gmail.com'
+		senha        = 'sua-senha'
+		 
+		# Informações da mensagem
+		destinatario = 'email-do-destinatario@qualquercoisa.com'
+		assunto      = 'Alerta de Foragido'
+		texto        = _mensagem
+		 
+		# Preparando a mensagem
+		msg = '\r\n'.join([
+		  'From: %s' % remetente,
+		  'To: %s' % destinatario,
+		  'Subject: %s' % assunto,
+		  '',
+		  '%s' % texto
+		  ])
+		 
+		# Enviando o email
+		server = smtplib.SMTP('smtp.gmail.com:587')
+		server.starttls()
+		server.login(remetente,senha)
+		server.sendmail(remetente, destinatario, msg)
+		server.quit()
--- models/camera.py
+++ models/camera.py
@@ -1,6 +1,7 @@
 class Camera(object):
 
-    def __init__(self, latitude, longitude, elevacao):
+    def __init__(self, latitude, longitude, elevacao, rua):
         self._latitude = latitude
         self._longitude = longitude
         self._elevacao = elevacao
+        self._rua = rua
