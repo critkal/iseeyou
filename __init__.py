b6ef3e953f7fa5c1406006d2dca2c0accdb0774b
Gabriel <gabriel-bro@outlook.com>
2018-06-26 14:15:53 -0300

    created main function

--- __init__.py
+++ __init__.py
@@ -1,6 +1,20 @@
+from controllers.detecter import DetectionController
+from controllers.DataController import DataController
+import os
 
 def main():
-    # my code here
+    ROOT_DIR = os.path.dirname(os.path.abspath('__init__.py'))
+    print (ROOT_DIR)
+    print ("downloading images from database")
+    data = DataController()
+    try:
+        data.downloadForagidos()
+    except Exception as identifier:
+        print identifier
+    os.chdir(ROOT_DIR)
+    cascPath = "haarcascade_frontalface_default.xml"
+    control = DetectionController()
+    control.accessWebCam(cascPath)
     return ""
 
 if __name__ == "__main__":
